5b2e86bbfdc3201d7867d53e770673b8b2d64bf9
mZabcic <mislav.zabcic@gmail.com>
2019-01-17 16:48:22 +0100

    Finish

--- wp-content/themes/wpbit4bytes/front-page.php
+++ wp-content/themes/wpbit4bytes/front-page.php
@@ -26,7 +26,7 @@ foreach($categories as $category) {
 $section_args = array(
   'post_type'   => 'post',
   'post_status' => 'publish',
-  'numberposts' => 4,
+  'posts_per_page' => 4,
   'category_name' => $category->slug
 
 );
